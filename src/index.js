462789fb3c68c402bd16143b16723e497a3b2494
Z3rbe <paul.monie@efrei.net>
2025-10-03 16:42:13 +0200

    firdt push test

--- src/index.js
+++ src/index.js
@@ -11,5 +11,5 @@ app.use('/', routes);
 
 const port = env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port} and ready to use !`);
 });
