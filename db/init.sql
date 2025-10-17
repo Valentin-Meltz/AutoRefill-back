f15332a475af59f3f474550497c3c19d61a87146
Z3rbe <paul.monie@efrei.net>
2025-10-17 15:56:28 +0200

    Last Update Full project

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE TABLE "User" (
+  id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  "firstName" VARCHAR(100) NOT NULL,
+  "lastName" VARCHAR(100) NOT NULL,
+  "address" VARCHAR(255) NOT NULL,
+  "password" VARCHAR(255) NOT NULL,
+  "postalCode" VARCHAR(20) NOT NULL,
+  "city" VARCHAR(100) NOT NULL,
+  "phone" VARCHAR(30) NOT NULL,
+  "email" VARCHAR(150) UNIQUE NOT NULL,
+  "role" VARCHAR(50) DEFAULT 'customer'
+);
+
